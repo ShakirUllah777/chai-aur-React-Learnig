32a41b8e2b4639b9e1b3df1f0d36a9bc352e3cea
turkish777 <shakir.turk777@gmail.com>
2026-01-22 18:49:09 +0500

    react mini heelo world app running

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1>Chai aur React</h1>
+    return (
+    <h1>Chai aur React with Vite</h1>
   )
 }
 
